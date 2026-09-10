fde3ac062e181ece22a2c8f2c5d42eeda693cd64
conanbke-ai <conanbke@gmail.com>
2026-09-10 17:25:16 +0900

    feat: load style preview UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import "./blotori-theme.css";
 import "./blotori-states.css";
 import "./blotori-ui-enhancements.css";
+import "./style-preview.css";
 import "./tori-paw-cursor.css";
 
 export const metadata: Metadata = {
